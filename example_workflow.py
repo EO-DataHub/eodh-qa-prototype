40ea1f5de64fce84d7b4713ac75ddef353df2793
sam.hunt <sam.hunt@npl.co.uk>
2023-10-10 13:28:41 +0100

    Init example workflow functions

--- example_workflow.py
+++ example_workflow.py
@@ -3,5 +3,27 @@
 __author__ = ["Sam Hunt <sam.hunt@npl.co.uk>", "Sam Malone <samantha.malone@npl.co.uk>"]
 __all__ = []
 
+
+def qacheck_geometry_coords_valid(item: dict) -> dict:
+    """
+    Checks that `"coordinates"` provided for item `"geometry"` entry valid.
+
+    :param item: STAC record for catalogue item
+    :return: check result
+    """
+
+    check_result = {}
+
+    return check_result
+
+
+def main():
+    """
+    Runs example QA check(s) for defined STAC item
+    """
+
+    return None
+
+
 if __name__ == "__main__":
-    pass
+    main()
