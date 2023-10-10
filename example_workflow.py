dbff39d0da7013153d3d3874aff55d99bbd3273a
sam.hunt <sam.hunt@npl.co.uk>
2023-10-10 13:17:07 +0100

    Init example workflow script

---
+++ example_workflow.py
@@ -0,0 +1,7 @@
+"""example_workflow - script to prototype qa check workflow for EODH"""
+
+__author__ = ["Sam Hunt <sam.hunt@npl.co.uk>", "Sam Malone <samantha.malone@npl.co.uk>"]
+__all__ = []
+
+if __name__ == "__main__":
+    pass
