a8181f98746a5333fec6efe0b741b83f6c6bb463
sam.hunt <sam.hunt@npl.co.uk>
2023-10-18 17:55:01 +0100

    move example check to class implementation

--- eodh_qa_prototype/example_workflow.py
+++ eodh_qa_prototype/example_workflow.py
@@ -1,12 +1,10 @@
 """example_workflow - script to prototype qa check workflow for EODH"""
 
-import requests
-import datetime as dt
 import json
+from eodh_qa_prototype.qa_check_workflows.stac_geometry_coordinates import QACheckSTACItemGeomCoords
 
 __author__ = ["Sam Hunt <sam.hunt@npl.co.uk>", "Sam Malone <samantha.malone@npl.co.uk>"]
 __all__ = []
-__version__ = "0.1"
 
 
 # url for example S2A product STAC item provided by CEDA
@@ -47,49 +45,6 @@ STAC_ITEM_JSON = json.dumps({"type": "Feature", "stac_version": "1.0.0", "stac_e
             "roles": ["thumbnail"]}}})
 
 
-def qacheck_geometry_coords_valid(item: dict) -> dict:
-    """
-    Checks that `"coordinates"` provided for item `"geometry"` entry valid.
-
-    :param item: STAC record for catalogue item
-    :return: check result
-    """
-
-    # Gets coordinate data from the example STAC item file
-    geometry_coords = item['geometry']['coordinates']
-
-    # Check values are within bounds for valid coordinate data
-    coords_valid = [False] * len(geometry_coords)
-    for i, coord_i in enumerate(geometry_coords):
-        if (-90 <= coord_i[0][0] <= 90) and (-180 <= coord_i[0][1] <= 180):
-            coords_valid[i] = True
-
-    valid_check = True if all(coords_valid) else False
-
-    # Date QA check was made as a string
-    date_of_check = dt.datetime.now().strftime("%Y%m%dT%H%M")
-
-    # Author of check
-    author = str(__author__)
-
-    # Link to reference site
-    reference_link = 'reference_url'
-
-    # Software version
-    software_version = __version__
-
-    # Dicionary including check and other metadata (version, software env etc)
-    check_result = {
-        "result": valid_check,
-        "datetime": date_of_check,
-        "author": author,
-        "reference_documents": reference_link,
-        "software_version": software_version
-    }
-
-    return check_result
-
-
 def main():
     """
     Runs example QA check(s) for defined STAC item
@@ -100,7 +55,8 @@ def main():
     stac_item = json.loads(STAC_ITEM_JSON)
 
     # Runs example QA check - this could have arbitrary complexity
-    example_check_result = qacheck_geometry_coords_valid(stac_item)
+    qa_check = QACheckSTACItemGeomCoords()
+    example_check_result, example_ancillary_info = qa_check.run_check(stac_item)
 
     return example_check_result
 
--- eodh_qa_prototype/qa_check_workflows/stac_geometry_coordinates.py
+++ eodh_qa_prototype/qa_check_workflows/stac_geometry_coordinates.py
@@ -1,10 +1,11 @@
 """eodh_qa_prototype.qa_check_workflows.stac_geometry_coordinates - validation for the STAC item geometry coordinates"""
 
 from typing import Tuple
+import datetime as dt
 from eodh_qa_prototype.qa_check_workflows.base import BaseQACheckWorkFlow
 
 __author__ = "Sam Hunt <sam.hunt@npl.co.uk>"
-__all__ = []
+__all__ = ["QACheckSTACItemGeomCoords"]
 
 
 class QACheckSTACItemGeomCoords(BaseQACheckWorkFlow):
@@ -12,4 +13,36 @@ class QACheckSTACItemGeomCoords(BaseQACheckWorkFlow):
     version = "1.0"
 
     def run_check(self, item: dict) -> Tuple[dict, dict]:
+        """
+        Checks that `"coordinates"` provided for STAC item `"geometry"` entry valid.
 
+        :param item: STAC record for catalogue item
+        :return: check result and ancillary info
+        """
+
+        # Gets coordinate data from the example STAC item file
+        geometry_coords = item['geometry']['coordinates']
+
+        # Check values are within bounds for valid coordinate data
+        coords_valid = [False] * len(geometry_coords)
+        for i, coord_i in enumerate(geometry_coords):
+            if (-90 <= coord_i[0][0] <= 90) and (-180 <= coord_i[0][1] <= 180):
+                coords_valid[i] = True
+
+        valid_check = all(coords_valid)
+
+        # Dicionary including check and other metadata (version, software env etc)
+        check_result = {
+            "result": valid_check,
+            "result_vocab": "vocab/url",
+            "check_datetime": dt.datetime.now().strftime("%Y%m%dT%H%M"),
+            "check_name": self.name,
+            "check_author": __author__,
+            "check_version": self.version,
+            "ref_url": {},
+        }
+
+        return check_result, {}
+
+if __name__ == "__main__":
+    pass
