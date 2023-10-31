d02fe4a3c912ad9f08ae1ef83408c92a29236010
sam.hunt <sam.hunt@npl.co.uk>
2023-10-31 11:42:45 +0000

    typo

--- eodh_qa_prototype/qa_check_workflows/__init__.py
+++ eodh_qa_prototype/qa_check_workflows/__init__.py
@@ -3,5 +3,5 @@ from eodh_qa_prototype.qa_check_workflows.stac_geometry_coordinates import QAChe
 __all__ = ["qa_check_workflow_registry"]
 
 qa_check_workflow_registry = {
-    QACheckSTACItemGeomCoords.name, QACheckSTACItemGeomCoords
+    QACheckSTACItemGeomCoords.name: QACheckSTACItemGeomCoords
 }
