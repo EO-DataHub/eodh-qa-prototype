d4caf4f097c159970e7b518da7f4752273bac11f
sam.hunt <sam.hunt@npl.co.uk>
2023-11-21 12:29:37 +0000

    init absolute radiometry check

--- eodh_qa_prototype/qa_check_workflows/__init__.py
+++ eodh_qa_prototype/qa_check_workflows/__init__.py
@@ -1,7 +1,9 @@
 from eodh_qa_prototype.qa_check_workflows.stac_geometry_coordinates import QACheckSTACItemGeomCoords
+from eodh_qa_prototype.qa_check_workflows.absolute_radiometry import AbsoluteRadiometry
 
 __all__ = ["qa_check_workflow_registry"]
 
 qa_check_workflow_registry = {
-    QACheckSTACItemGeomCoords.name: QACheckSTACItemGeomCoords
+    QACheckSTACItemGeomCoords.name: QACheckSTACItemGeomCoords,
+    AbsoluteRadiometry.name: AbsoluteRadiometry
 }
